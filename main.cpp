50af4a98f3d1e698edea5bc10c2c550cdf473c3c
218552 <218552@student.pwr.edu.pl>
2016-03-31 22:50:18 +0200

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -7,7 +7,7 @@ using namespace std;
 class Interfejs
 {
 public:
-  virtual void stoper(bool flag)=0;
+  virtual double stoper(bool flag)=0;
 };
 
 class Interfejs2
@@ -15,23 +15,27 @@ class Interfejs2
 {
   clock_t start;
 public:
-  void stoper(bool flag);
+  double stoper(bool flag);
+  bool *zmien_rozmiar(bool *tab,int n,int zwieksz);
 };
   
-void Interfejs2::stoper(bool flag)
+double Interfejs2::stoper(bool flag)
 {
   if(flag==1)
-    start=clock();
+    {
+      start=clock();
+      return 0;
+    }
   else
-    cout<<"Czas wykonania algorytmu: "<<double(clock()-start)/(double)CLOCKS_PER_SEC<<"s"<<endl;
+    return double(clock()-start)/(double)CLOCKS_PER_SEC;
 }
 
-/* Funkcja zwieksza rozmiar tablicy o zadana wartosc           */
+/* Metoda zwieksza rozmiar tablicy o zadana wartosc           */
 /* Parametry                                                   */
 /* tab - tablica                                               */
 /* n - rozmiar tablicy przed powiekszeniem                     */
 /* zwieksz - ilosc elementow o jaka tablica zostanie zwiekszona*/
-bool *zmien_rozmiar(bool *tab,int n,int zwieksz)
+bool* Interfejs2::zmien_rozmiar(bool *tab,int n,int zwieksz)
 {
   bool *tmp=new bool[n];
   
@@ -59,10 +63,10 @@ int main()
   Interfejs2 inter;
   int n=10;              //poczatkowa ilosc elementow tablicy
   int liczba=1;          //liczba do przypisania wartosci elementom tablicy
-  int zwieksz=10;         //ilosc elementow o jaka zwiekszana bedzie tablica
+  int zwieksz=100;         //ilosc elementow o jaka zwiekszana bedzie tablica
   bool *tab=new bool[n];
   inter.stoper(1);
-  for(int i=0;i<200000;i++)
+  for(int i=0;i<100000;i++)
     {
       if(i<n)
 	{
@@ -70,11 +74,11 @@ int main()
 	}
       else
 	{
-	  tab=zmien_rozmiar(tab,n,zwieksz);
+	  tab=inter.zmien_rozmiar(tab,n,zwieksz);
 	  tab[i]=liczba;
 	  n=n+zwieksz;
 	}
-    }
+    }   
+  inter.stoper(0);	
   delete[] tab;
-  inter.stoper(0);
 }
