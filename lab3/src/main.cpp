7872c0b5caf139a3aed4cf25cd3ebd3a7ddb92b2
218552 <218552@student.pwr.edu.pl>
2016-04-12 15:44:56 +0200

    Added files via upload

--- lab3/src/main.cpp
+++ lab3/src/main.cpp
@@ -14,13 +14,14 @@ int main()
   List lista;
   Stoper stoper;
   srand(time(NULL));
-  // Poczatek pomiaru czasu
-  stoper.measure_time(1);
+
   // Wypelnienie listy
   for(int i=1;i<=100000000;i++)
     {
       lista.add(rand()%101,i);
     }
+  // Poczatek pomiaru czasu
+  stoper.measure_time(1);
   // Wyszukanie danego elementu z listy
   lista.search(100);
   // Koniec pomiaru czasu i wyswietlenie wyniku
